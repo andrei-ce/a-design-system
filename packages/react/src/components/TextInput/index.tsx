a55039de46214ff695168abf6eb37ae424e0aa3c
Andrei Ce <andrei90@gmail.com>
2023-07-24 16:11:46 -0300

    fix: referring to right github path after deploy

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -5,7 +5,7 @@ export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
 }
 
-// we need fowardRef because we need to access the ref var to be able to access the value and it's properties like focus, etc.
+// We need fowardRef because we need to access the ref var to be able to access the value and it's properties like focus, etc.
 // ElementRef extracts the typing for a react element
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
   ({ prefix, ...props }: TextInputProps, ref) => {
