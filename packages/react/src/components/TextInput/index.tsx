f2c3b12cebc1019dc46fff38796059fcd95889a8
Andrei Ce <andrei90@gmail.com>
2023-08-01 16:07:01 -0300

    feat: small changes to be able to use this design system properly on the CALME app

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,16 +1,18 @@
 import { ComponentProps, forwardRef, ElementRef } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
 
-export interface TextInputProps extends ComponentProps<typeof Input> {
+export interface TextInputProps
+  extends Omit<ComponentProps<typeof Input>, 'size'> {
   prefix?: string
+  size?: ComponentProps<typeof TextInputContainer>['size']
 }
 
 // We need fowardRef because we need to access the ref var to be able to access the value and it's properties like focus, etc.
 // ElementRef extracts the typing for a react element
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
-  ({ prefix, ...props }: TextInputProps, ref) => {
+  ({ prefix, size, ...props }, ref) => {
     return (
-      <TextInputContainer>
+      <TextInputContainer size={size}>
         {!!prefix && <Prefix>{prefix}</Prefix>}
         <Input ref={ref} {...props} />
       </TextInputContainer>
